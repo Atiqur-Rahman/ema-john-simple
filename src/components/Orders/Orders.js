2196eca06b407943653076d4b1b89d2589a4b92f
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-13 15:58:20 +0600

    order component is done

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+    const handleRemoveProduct = (product) => {
+        const rest = cart.filter((pd) => pd.id !== product.id);
+        setCart(rest);
+        removeFromDb(product.id);
+    };
     return (
         <div>
             <div className="shop-container">
-                <div className="product-container">
+                <div className="review-items-container">
                     {cart.map((product) => (
-                        <ReviewItem key={product.id} product={product}></ReviewItem>
+                        <ReviewItem key={product.id} product={product} handleRemoveProduct={handleRemoveProduct}></ReviewItem>
                     ))}
                 </div>
                 <div className="cart-container">
-                    <Cart cart={cart}></Cart>
+                    <Cart cart={cart}>
+                        <Link to="/inventory">
+                            <button>Proceed Checkout</button>
+                        </Link>
+                    </Cart>
                 </div>
             </div>
         </div>
