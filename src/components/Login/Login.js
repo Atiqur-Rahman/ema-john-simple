7d87e41f49a025866819d32ad8fddb7744bb6d8c
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-29 14:16:38 +0600

    Style sign up component and access email, password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,20 +10,27 @@ const Login = () => {
                 <form>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input type="email" name="email" id="" required />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input type="password" name="password" id="" required />
                     </div>
                     <input className="form-submit" type="submit" value="Login" />
                 </form>
-                <p>
-                    New to Ema-John?{' '}
+                <p className="form-confirmation">
+                    New to Ema-John?
                     <Link className="form-link" to="/signup">
                         Create an account
                     </Link>
                 </p>
+                <p className="line-style">
+                    <span>or</span>
+                </p>
+                <button style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer' }}>
+                    <img style={{ width: '32px', paddingRight: '8px' }} src={require('../../google.png')} alt="" />
+                    Continue with Google
+                </button>
             </div>
         </div>
     );
