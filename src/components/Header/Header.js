f27ea0cdc1dffdb57616ee50c5d2502c60f2d966
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-30 09:47:30 +0600

    Implement Sign out and Create a shipment form

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSignOut } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
 import logo from '../../images/Logo.svg';
@@ -7,6 +7,11 @@ import './Header.css';
 
 const Header = () => {
     const [user] = useAuthState(auth);
+    const [signOut] = useSignOut(auth);
+    const handleSignOut = () => {
+        signOut();
+    };
+
     return (
         <nav className="header">
             <img src={logo} alt="" />
@@ -15,7 +20,7 @@ const Header = () => {
                 <Link to="/orders">Orders</Link>
                 <Link to="/inventory">Inventory</Link>
                 <Link to="/about">about</Link>
-                {user ? <button>Sign Out</button> : <Link to="/login">Login</Link>}
+                {user ? <button onClick={handleSignOut}>Sign Out</button> : <Link to="/login">Login</Link>}
             </div>
         </nav>
     );
