b3d4bdda1cc8b1f28a05c5405624ba31a7c1f74b
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-30 10:56:06 +0600

    updated shipping form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
 
     const navigate = useNavigate();
     const location = useLocation();
-    const from = location?.state?.from?.pathname || '/';
+    const from = location.state?.from?.pathname || '/';
 
     if (user) {
         navigate(from, { replace: true });
